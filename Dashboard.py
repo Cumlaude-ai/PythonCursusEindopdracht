21c3953f18fb0c0fc76b45279bb43442a5d8fb7f
Bram Teunis <141015845+BramTeunisCumlaude@users.noreply.github.com>
2023-10-15 10:40:52 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -269,4 +269,4 @@ def createDashboard(Title,SiteUrl,LogoUrl,BackgroundUrl,Data,Graphs):
     
     app.layout = html.Div(children=[NAVBAR, BODY])
         
-    return app.run(jupyter_mode="external",debug=False), output.serve_kernel_port_as_iframe(8050)
+    return app.run(jupyter_mode="external",debug=True), output.serve_kernel_port_as_iframe(8050)
